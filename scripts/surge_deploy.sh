7139cf9700e7a343afdbf8d27d6f45b870d12d71
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2020-01-15 13:30:17 +0530

    Configure surge auto-deployment

---
+++ scripts/surge_deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "Not a PR. Skipping surge deployment"
+    exit 0
+fi
+# Check if the travis credentials exists
+if [ -z "$SURGE_LOGIN" ] || [ -z "$SURGE_TOKEN" ]; then
+    echo "Surge credentials not set. Skipping surge deployment"
+    exit 0
+fi
+# Install surge
+npm i -g surge
+cd src/main/webapp
+# Create a domain to the PR
+export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-sef-site.surge.sh
+# Upload to surge.sh
+surge --project ./build --domain $DEPLOY_DOMAIN;
