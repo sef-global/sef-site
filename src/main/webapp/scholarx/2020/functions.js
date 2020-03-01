aed920908a7ccdd54c47b00007b1a4e10cb196af
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2020-03-01 15:29:42 +0530

    Fix resposnive issues of schoalrX 2020 page (#568)

--- src/main/webapp/scholarx/2020/functions.js
+++ src/main/webapp/scholarx/2020/functions.js
@@ -7,6 +7,25 @@ $(function () {
 
     loadProfiles(); // load profiles
     loadFeaturedStories(); // load featured stories
+
+    // Listner for FAQ
+    $('.panel').click(
+        function () {
+            if($(this).hasClass("collapsed")){
+                const collapseSign = $('.plusMinusSign');
+                collapseSign.removeClass("fa-minus");
+                collapseSign.addClass("fa-plus");
+                $(this).children("div").children("i").removeClass("fa-plus");
+                $(this).children("div").children("i").addClass("fa-minus");
+                $('.card-toggle').removeClass("card shadow");
+                $(this).parent("div").parent("div").addClass("card shadow");
+            }else{
+                $(this).parent("div").parent("div").removeClass("card shadow");
+                $(this).children("div").children("i").removeClass("fa-minus");
+                $(this).children("div").children("i").addClass("fa-plus");
+            }
+        }
+    );
 });
 
 //function to load profile data
@@ -105,3 +124,4 @@ function openMentorProfile(index){
     // Open the modal
     $('#profile-modal').modal();
 }
+
