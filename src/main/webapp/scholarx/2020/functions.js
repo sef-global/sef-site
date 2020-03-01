e644971628553cf95f90b104864f022baa24400c
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2020-03-01 18:51:35 +0530

    Merge scholarx-2020 to the master (#575)
    * Reorganize newly added fields in scholarx.json (#526)
    
    * Revert "Add new fields to the mentors in scholarx. (#517)" (#525)
    
    This reverts commit 0c7d71c11b0b6c19a8e413a1e4efed377f6ec26a.
    
    * Reorganize newly added fields in scholarx.json and added content
    
    Co-authored-by: Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
    
    * Revert "Reorganize newly added fields in scholarx.json (#526)" (#530)
    
    This reverts commit b964c346411eac532942156f032c7a191c603e71.
    
    * Change the file structure of the scholarx pages (#534)
    
    * Change description of scholarx page (#536)
    
    * Add a program timeline to the scholarx page. (#535)
    
    * Reorganize newly added fields in scholarx.json and add content (#537)
    
    * Reorganize newly added fields in scholarx.json and add content
    
    * Reorganize newly added fields in scholarx.json and add content
    
    * Reorganize newly added fields in scholarx.json and add content
    
    * Reorganize newly added fields in scholarx.json and add content
    
    * Replace current FAQ's for mentors. (#538)
    
    * Replace current FAQ's for mentors.
    
    * Replace current FAQ's for mentors.
    
    * Update Find a mentor for me section in scholarx (#542)
    
    Change following things
    1. Link for apply https://docs.google.com/forms/d/1rVN3fOy9YhTad1_uuDaoL30R-YM0IGI12Yu3I9W1vng/edit
    
    2. Deadline 14th April 2020
    
    * Replace current FAQ's for mentees. (#547)
    
    * Create a empty profile modal for mentors (#546)
    
    * Create a section to view featured stories of scholarx 2020 (#552)
    
    * Add a section on 2020 scholarX page to display past scholarX programs (#553)
    
    * Create a basic page to display past scholarX. (#549)
    
    * Create a basic page to display past scholarX.
    
    * Add photos for the missing mentors
    
    * Add navbar and the footer to the scholarX archive page (#561)
    
    * Design the profile card for 2020 scholarX mentors (#550)
    
    * Fix mentor loading issue in ScholarX page (#562)
    
    * Load profile modals dynamically using mentor data (#564)
    
    * Change the order of the content of scholarX 2020 page
    
    * Update the hero description of the ScholarX archive page
    
    * Fix resposnive issues of schoalrX 2020 page (#568)
    
    * Rename button in the find a mentor section in the ScolarX 2020 page
    
    * Add apply button to the hero section (#572)
    
    * Add apply button to the hero section
    
    * Add apply button to the hero section
    
    * Add requested text changes
    
    Co-authored-by: Anjula Samarasinghe <anjulashanaka@gmail.com>
    Co-authored-by: Janeth Fernando <janethavishka@gmail.com>
    Co-authored-by: Suthan97 <60068030+Suthan97@users.noreply.github.com>
    Co-authored-by: AselSiriwardena <35985674+AselSiriwardena@users.noreply.github.com>

---
+++ src/main/webapp/scholarx/2020/functions.js
@@ -0,0 +1,127 @@
+// Write custom js functions for the scholarx 2020 page here
+
+let mentorProfiles = [];
+
+$(function () {
+    loadNavAndFooter('/assets/content/static');  //relative path to content directory
+
+    loadProfiles(); // load profiles
+    loadFeaturedStories(); // load featured stories
+
+    // Listner for FAQ
+    $('.panel').click(
+        function () {
+            if($(this).hasClass("collapsed")){
+                const collapseSign = $('.plusMinusSign');
+                collapseSign.removeClass("fa-minus");
+                collapseSign.addClass("fa-plus");
+                $(this).children("div").children("i").removeClass("fa-plus");
+                $(this).children("div").children("i").addClass("fa-minus");
+                $('.card-toggle').removeClass("card shadow");
+                $(this).parent("div").parent("div").addClass("card shadow");
+            }else{
+                $(this).parent("div").parent("div").removeClass("card shadow");
+                $(this).children("div").children("i").removeClass("fa-minus");
+                $(this).children("div").children("i").addClass("fa-plus");
+            }
+        }
+    );
+});
+
+//function to load profile data
+function loadProfiles() {
+    $.ajax({
+        type: 'get',
+        url: 'mentors.json',
+        dataType: 'json',
+        success: function (profiles) {
+            mentorProfiles = profiles;
+            // Add a new key named `index` with the index (To use with moustache template)
+            profiles.forEach(function (profile, index) {
+                profile.index = index;
+            });
+            //slice array to two parts
+            if (profiles.length >= 8) {
+                let partOne = profiles.slice(0, 8);
+                let partTwo = profiles.slice(8);
+
+                //mustache render - part one
+                let contentPartOne = Mustache.render($("#templateTeam").html(), {"profiles": partOne});
+
+                //display first 8 profiles
+                $("#teamContent").html(contentPartOne);
+
+                //hide button
+                $("#btnShowMore").click(function () {
+                    let contentPartTwo = Mustache.render($("#templateTeam").html(), {"profiles": partTwo});
+                    $(contentPartTwo).appendTo("#teamContent").hide().fadeIn(1000);
+                    $("#btnShowMore").hide();
+                });
+            } else {
+                //mustache render
+                let content = Mustache.render($("#templateTeam").html(), {"data": profiles});
+
+                //display first 8 profiles
+                $("#teamContent").html(content);
+
+                //hide button
+                $("#btnShowMore").hide();
+            }
+        }
+    });
+}
+
+//function to load profile data
+function loadFeaturedStories() {
+    $.ajax({
+        type: 'get',
+        url: 'featured-stories.json',
+        dataType: 'json',
+        success: function (data) {
+
+            //slice array to two parts
+            if (data.length >= 6) {
+                let partOne = data.slice(0, 6);
+                let partTwo = data.slice(6);
+
+                //mustache render - part one
+                let contentPartOne = Mustache.render(
+                    $('#template-featured-stories').html(), {'data': partOne});
+
+                //display first 8 profiles
+                $('#featured-stories').html(contentPartOne);
+
+                //hide button
+                $('#btn-featured-stories-show-more').click(function () {
+                    let contentPartTwo = Mustache.render(
+                        $('#template-featured-stories').html(), {'data': partTwo});
+                    $(contentPartTwo).appendTo('#featured-stories').hide().fadeIn(1000);
+                    $('#btn-featured-stories-show-more').hide();
+                });
+            } else {
+                //mustache render
+                let content = Mustache.render(
+                    $('#template-featured-stories').html(), {'data': data});
+
+                //display first 8 profiles
+                $('#featured-stories').html(content);
+
+                //hide button
+                $('#btn-featured-stories-show-more').hide();
+            }
+        }
+    });
+}
+
+function openMentorProfile(index){
+    // Get the selected mentor
+    const profile = mentorProfiles[parseInt(index)];
+    // Add content to the template
+    const template = $('#template-profile-modal-content').html();
+    const content = Mustache.render(template, profile);
+    // Update the modal
+    $('#profile-modal-content').html(content);
+    // Open the modal
+    $('#profile-modal').modal();
+}
+
