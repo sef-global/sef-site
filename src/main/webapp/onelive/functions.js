156473fe6a64923763f4a8938b9ce4a87a6ae375
anjula-sack <anjulashanaka@gmail.com>
2020-05-04 13:29:38 +0530

    Remove date from onelive video component

--- src/main/webapp/onelive/functions.js
+++ src/main/webapp/onelive/functions.js
@@ -59,9 +59,6 @@ function loadYoutubeVideos() {
                 if (snippet.description.length > 100) {
                     snippet.description = snippet.description.slice(0, 80) + "...";
                 }
-                let date = snippet.publishedAt;
-                date = moment(date).format('LL');
-                snippet.publishedAt = date;
             });
             // Mustache render
             let renderedData = Mustache.render(
