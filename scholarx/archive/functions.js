b7a833bc5ffba406c1e5a0a1870ffc10f16f7c37
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-11-26 18:07:17 +0530

    Move menteesOfAMentor function to AppsScript

--- scholarx/archive/functions.js
+++ scholarx/archive/functions.js
@@ -2,7 +2,7 @@ $(function () {
     loadNavAndFooter('/assets/content/static');  //relative path to content directory
 });
 
-const data_url = "https://script.google.com/macros/s/AKfycbzOQyrs0M8uN5ztxhRm2GMKlNkBaul_oaH2i4kiSKieyZtVzeNlvdR__HLveBxNU86S/exec";
+const data_url = "https://script.google.com/macros/s/AKfycbw0TVyldiK5ijUxjLJkhrxHpxZpIjoeLytZqrvOlftYeHywn1hBEnL0aHGIS8hOFxPp/exec";
 let mentors2021 = [];
 let mentors2020 = [];
 let mentors2019 = [];
@@ -14,41 +14,13 @@ async function getData() {
     const response = await fetch(data_url);
     const data = await response.json();
 
-    mentors2021 = data.mentor2021;
-    mentees2021 = data.mentee2021;
-    mentors2021.forEach(mentor => {
-        menteesOfMentor = []
-        mentees2021.forEach(mentee => {
-            if (mentee.mentor == mentor.name) {
-                menteesOfMentor.push(mentee.name);
-            }
-        });
-        mentor.mentees = menteesOfMentor;
-    });
-
-    mentors2020 = data.mentor2020;
-    mentees2020 = data.mentee2020;
-    mentors2020.forEach(mentor => {
-        menteesOfMentor = []
-        mentees2020.forEach(mentee => {
-            if (mentee.mentor == mentor.name) {
-                menteesOfMentor.push(mentee.name);
-            }
-        });
-        mentor.mentees = menteesOfMentor;
-    });
-
     mentors2019 = data.mentor2019;
     mentees2019 = data.mentee2019;
-    mentors2019.forEach(mentor => {
-        menteesOfMentor = []
-        mentees2019.forEach(mentee => {
-            if (mentee.mentor == mentor.name) {
-                menteesOfMentor.push(mentee.name);
-            }
-        });
-        mentor.mentees = menteesOfMentor;
-    });
+    mentors2020 = data.mentor2020;
+    mentees2020 = data.mentee2020;
+    mentors2021 = data.mentor2021;
+    mentees2021 = data.mentee2021;
+    
 }
 
 async function loadData() {
