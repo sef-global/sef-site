f2169a3fb884659169a5dca87a0525b21d0eeb20
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2022-12-04 12:46:30 +0530

    Add mentee section with search functionality (#1440)
    * Fix viewing issue on different devices
    
    * Commented out the Reading time Section
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Fixed error in program timeline
    
    * Remove inline css
    
    * Fixed timeline section of the past ScholarX programs
    
    * Moved CSS code to global-custom.css file
    
    * Update global-custom.css
    
    * Fix Overlapping issues in FAQ Mentor/Mentee Section
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update global-custom.css
    
    * Fix overlap issue in AcadeMiX page
    
    * Fixed Image Overlapping Issue
    
    * Added Comment
    
    * Update index.html
    
    * Change the navbar comment
    
    * Update the background image comment
    
    * Update index.html
    
    * Added mentee section with search
    
    * Remove unwanted lines
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * rename variables.
    
    * move javascript to function.js file
    
    * change id from topic2 to title
    
    * Update functions.js
    
    Co-authored-by: YOSHITHA RATHNAYAKE <yoshithasbrathnayake@gmail.com>

--- scholarx/archive/functions.js
+++ scholarx/archive/functions.js
@@ -2,6 +2,43 @@ $(function () {
     loadNavAndFooter('/assets/content/static');  //relative path to content directory
 });
 
+//search mentors and mentees
+$(document).ready(function () {
+    $("#search").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#mentorProfiles tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+        $("#menteeProfiles tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    $$('div.tags').find('input:checkbox').live('click', function () {
+        $('#mentorProfiles > tr').hide();
+        $('#menteeProfiles > tr').hide();
+        $('div.tags').find('input:checked').each(function () {
+            $('#mentorProfiles > tr.' + $(this).val()).show();
+        });
+        $('div.tags').find('input:checked').each(function () {
+            $('#menteeProfiles > tr.' + $(this).val()).show();
+        });
+    });
+});
+
+//mentor mentee transition
+$(document).ready(function(){
+    $('#selection').on('change', function(){
+    	var selectedValue = $(this).val();
+        if(selectedValue === "mentees"){
+            document.getElementById("showMentors").style.display = "none";
+            document.getElementById("showMentees").style.display = "flex";
+        } else if(selectedValue === "mentors"){
+            document.getElementById("showMentees").style.display = "none";
+            document.getElementById("showMentors").style.display = "flex";
+        }      
+    });
+});
+
 const data_url = "https://script.google.com/macros/s/AKfycbw0TVyldiK5ijUxjLJkhrxHpxZpIjoeLytZqrvOlftYeHywn1hBEnL0aHGIS8hOFxPp/exec";
 
 async function getData() {
@@ -13,7 +50,11 @@ async function getData() {
 async function loadData() {
     const payload = await getData();
     const mentorProfiles = payload.mentor2021.concat(payload.mentor2020).concat(payload.mentor2019);
-    let content = Mustache.render($("#templateMentors").html(), { "mentorProfiles": mentorProfiles });
-    $("#team").html(content);
+    let mentor_Profiles = Mustache.render($("#templateMentors").html(), { "mentorProfiles": mentorProfiles });
+    $("#mentorProfiles").html(mentor_Profiles);
+
+    const menteeProfiles = payload.mentee2021.concat(payload.mentee2020).concat(payload.mentee2019);
+    let mentee_Profiles = Mustache.render($("#templateMentees").html(), { "menteeProfiles": menteeProfiles });
+    $("#menteeProfiles").html(mentee_Profiles);
 }
 loadData();
