f90ff9431e2b99c88f93413f114c23affb252d1e
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2022-12-08 00:54:01 +0530

    Update functions.js

--- scholarx/archive/functions.js
+++ scholarx/archive/functions.js
@@ -3,8 +3,8 @@ $(function () {
 });
 
 //global variables to hold mentor and mentee data
-var mentor2019; var mentor2020; var mentor2021; var mentor20202021; var mentor20192020; var mentor20192021;
-var mentee2019; var mentee2020; var mentee2021; var mentee20202021; var mentee20192020; var mentee20192021;
+var mentor2019, mentor2020, mentor2021;
+var mentee2019, mentee2020, mentee2021;
 
 //search mentors and mentees
 $(document).ready(function () {
@@ -53,12 +53,12 @@ async function getData() {
 
 async function loadData() {
     const payload = await getData();
-    const m2019 = payload.mentor2019; const m2020 = payload.mentor2020; const m2021 = payload.mentor2021;
-    const n2019 = payload.mentee2019; const n2020 = payload.mentee2020; const n2021 = payload.mentee2021;
+    const m2019 = payload.mentor2019, m2020 = payload.mentor2020, m2021 = payload.mentor2021;
+    const n2019 = payload.mentee2019, n2020 = payload.mentee2020, n2021 = payload.mentee2021;
 
     //save mentor mentee details in global array variables to faster data access.
-    [mentor2019,mentor2020,mentor2021,mentor20192020,mentor20202021,mentor20192021] = [m2019,m2020,m2021,m2020.concat(m2021),m2020.concat(m2021),m2019.concat(m2021)];
-    [mentee2019,mentee2020,mentee2021,mentee20192020,mentee20202021,mentee20192021] = [n2019,n2020,n2021,n2020.concat(n2019),n2020.concat(n2021),n2019.concat(n2021)];
+    [mentor2019,mentor2020,mentor2021] = [m2019,m2020,m2021];
+    [mentee2019,mentee2020,mentee2021] = [n2019,n2020,n2021];
  
     const mentorProfiles = m2019.concat(m2020).concat(m2021);
     let mentor_Profiles = Mustache.render($("#templateMentors").html(), { "mentorProfiles": mentorProfiles });
@@ -94,11 +94,11 @@ function cohort(){
     } else if(document.getElementById("chk2019").checked == false && document.getElementById("chk2020").checked == false && document.getElementById("chk2021").checked == true){
         renderData(mentor2021,mentee2021);
     } else if(document.getElementById("chk2019").checked == false && document.getElementById("chk2020").checked == true && document.getElementById("chk2021").checked == true){
-        renderData(mentor20202021,mentee20202021);
+        renderData(mentor2020.concat(mentor2021),mentee2020.concat(mentee2021));
     } else if(document.getElementById("chk2019").checked == true && document.getElementById("chk2020").checked == true && document.getElementById("chk2021").checked == false){
-        renderData(mentor20192020,mentee20192020);
+        renderData(mentor2019.concat(mentor2020),mentee2019.concat(mentee2020));
     } else if(document.getElementById("chk2019").checked == true && document.getElementById("chk2020").checked == false && document.getElementById("chk2021").checked == true){
-        renderData(mentor20192021,mentee20192021);
+        renderData(mentor2019.concat(mentor2021),mentee2019.concat(mentee2021));
     } else{
         renderAll();
     }
